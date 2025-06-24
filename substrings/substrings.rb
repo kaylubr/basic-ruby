2cc5dd282ef0088537c42e7f31e35708c89456a5
kaylubr <kylebreyes092005@gmail.com>
2025-06-24 10:46:59 +0800

    Refractor: removed the unecessary return expression

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -2,9 +2,7 @@ def substrings(word, dictionary)
   word_arr = word.split(" ")
   word_arr.reduce(Hash.new(0)) do |acc, curr|
     dictionary.each do |value|
-      if curr.downcase.include?(value.downcase)
-        acc[value] += 1
-      end
+        acc[value] += 1 if curr.downcase.include?(value.downcase)
     end
     acc
   end
